fcab98c4b4d2c1c3177e459643f2d79865c80e13
sperezlasa <SPerez@laaurora.do>
2023-08-25 16:26:03 -0400

    testing action 6

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,24 +1,24 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { AppModule } from './app.module';
+// import { Test, TestingModule } from '@nestjs/testing';
+// import { AppController } from './app.controller';
+// import { AppService } from './app.service';
+// import { AppModule } from './app.module';
 
-describe('AppController', () => {
-  // let appController: AppController;
+// describe('AppController', () => {
+//   // let appController: AppController;
 
-  beforeEach(async () => {
-    const app: TestingModule = await Test.createTestingModule({
-      controllers: [AppController],
-      imports: [AppModule],
-      providers: [AppService],
-    }).compile();
+//   beforeEach(async () => {
+//     const app: TestingModule = await Test.createTestingModule({
+//       controllers: [AppController],
+//       imports: [AppModule],
+//       providers: [AppService],
+//     }).compile();
 
-    // appController = app.get<AppController>(AppController);
-  });
+//     // appController = app.get<AppController>(AppController);
+//   });
 
-  // describe('root', () => {
-  //   it('should return "Hello World!"', () => {
-  //     expect(appController.getHello()).toBe('Hello World!');
-  //   });
-  // });
-});
+//   // describe('root', () => {
+//   //   it('should return "Hello World!"', () => {
+//   //     expect(appController.getHello()).toBe('Hello World!');
+//   //   });
+//   });
+// });
